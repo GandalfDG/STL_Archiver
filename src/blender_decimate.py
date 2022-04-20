c29ee181d962b9f4f222083bfbed9c01312785e4
Jack Case <jackacase@gmail.com>
2022-04-20 10:00:05 -0400

    create skeleton of blender python script to decimate STLs

---
+++ src/blender_decimate.py
@@ -0,0 +1,26 @@
+"""Pass a list of filenames or a glob pattern in along with a value indicating
+the decimation ratio, get decimated STLs back out saving lots of space and not
+really compromising quality for teeny-tiny 3D prints
+"""
+
+from sys import argv
+
+import bpy
+
+def load_model():
+    pass
+
+def normalize_model():
+    pass
+
+def decimate_model():
+    pass
+
+def export_model():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
